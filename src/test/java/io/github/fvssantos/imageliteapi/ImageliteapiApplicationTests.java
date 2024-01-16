b04f8612c533ce7acc11323751599a5f68d66546
Flávio Vinícius <84485936+FVSSANTOS@users.noreply.github.com>
2024-01-16 16:51:22 -0300

    Add files via upload

---
+++ src/test/java/io/github/fvssantos/imageliteapi/ImageliteapiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.fvssantos.imageliteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageliteapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
